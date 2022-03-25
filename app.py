c1508da3ee0b4ddffe242265c9971669cc461c6a
dsparetroyard <monodspr@gmail.com>
2022-03-26 00:38:26 +0500

    html pages added for part one, part two. part one done

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def page_index():
-    test = "test test"
-    return render_template('index.html', test=test)
+    return render_template('index.html')
+
+
+@app.route('/part_one')
+def page_part_one():
+    return render_template('part_one.html')
+
+
+@app.route('/part_two')
+def page_part_two():
+    return render_template('part_two.html')
 
 
 app.run()
